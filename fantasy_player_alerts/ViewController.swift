0d7f54ff044e6bc13fbed5907fa3a9008d8ae561
Shane Brosnan <shanebrosnan@shanes-macbook-pro.local>
2019-01-02 11:36:20 -0500

    Initial Commit

---
+++ fantasy_player_alerts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fantasy_player_alerts
+//
+//  Created by Shane Brosnan on 1/2/19.
+//  Copyright © 2019 Shane Brosnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
